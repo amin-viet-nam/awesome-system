8067265a6f9303c7f5a36156dcfd57907548e4ac
Minh Nguyen <minhnv02@acb.com.vn>
2019-09-14 15:55:55 +0700

    update new cert

--- k8s/00-generic/config.sh
+++ k8s/00-generic/config.sh
@@ -4,26 +4,26 @@ CONFIG_DIR=`dirname $0`
 
 
 # use this to renew let's encrypt ssl for *.anhminhltd.com
-brew install certbot
+# brew install certbot
 
 # use this to create certificate
-# certbot certonly --manual \
-# 	--preferred-challenges dns \
-# 	--email developer@anhminhltd.com \
-# 	--server https://acme-v02.api.letsencrypt.org/directory \
-# 	--agree-tos \
-# 	--config-dir $EXECUTE_DIR \
-# 	--work-dir $EXECUTE_DIR \
-# 	--logs-dir $EXECUTE_DIR \
-# 	-d *.anhminhltd.com
-
-certbot renew \
+certbot certonly --manual \
+	--preferred-challenges dns \
+	--email developer@anhminhltd.com \
+	--server https://acme-v02.api.letsencrypt.org/directory \
+	--agree-tos \
 	--config-dir $EXECUTE_DIR \
 	--work-dir $EXECUTE_DIR \
 	--logs-dir $EXECUTE_DIR \
+	-d *.anhminhltd.com
 
-cp $EXECUTE_DIR/live/anhminhltd.com-0001/fullchain.pem anhminhltd.com.crt
-cp $EXECUTE_DIR/live/anhminhltd.com-0001/privkey.pem anhminhltd.com.key
+# certbot renew \
+# 	--config-dir $EXECUTE_DIR \
+# 	--work-dir $EXECUTE_DIR \
+# 	--logs-dir $EXECUTE_DIR \
+
+cp $EXECUTE_DIR/live/anhminhltd.com/fullchain.pem anhminhltd.com.crt
+cp $EXECUTE_DIR/live/anhminhltd.com/privkey.pem anhminhltd.com.key
 
 kubectl create secret generic istio-ingressgateway-certs \
 	--from-file=$CONFIG_DIR/anhminhltd.com.crt \
